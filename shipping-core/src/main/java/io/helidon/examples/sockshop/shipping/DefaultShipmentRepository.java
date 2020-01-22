089c2abacc19af975c4701c7f35c00470f42f917
Aleksandar Seovic <aleks@seovic.com>
2020-01-22 06:51:02 -0500

    Fixed beans.xml and bean annotations

--- shipping-core/src/main/java/io/helidon/examples/sockshop/shipping/DefaultShipmentRepository.java
+++ shipping-core/src/main/java/io/helidon/examples/sockshop/shipping/DefaultShipmentRepository.java
@@ -5,12 +5,10 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.inject.Alternative;
 
 /**
  */
 @ApplicationScoped
-@Alternative
 public class DefaultShipmentRepository implements ShipmentRepository {
 
     private Map<String, Shipment> shipments = new ConcurrentHashMap<>();
