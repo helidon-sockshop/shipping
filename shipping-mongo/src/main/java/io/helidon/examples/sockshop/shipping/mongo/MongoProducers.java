c3c92d4b5a351f5a0d0a728193686b50877c8f99
Aleksandar Seovic <aleks@seovic.com>
2020-01-22 06:23:15 -0500

    Added MongoDB-backed Orders, Payment, and Shipping services

---
+++ shipping-mongo/src/main/java/io/helidon/examples/sockshop/shipping/mongo/MongoProducers.java
@@ -0,0 +1,55 @@
+package io.helidon.examples.sockshop.shipping.mongo;
+
+import java.util.Collections;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
+
+import io.helidon.examples.sockshop.shipping.Shipment;
+
+import com.mongodb.MongoClientSettings;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.Conventions;
+import org.bson.codecs.pojo.PojoCodecProvider;
+
+import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
+import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
+
+/**
+ */
+@ApplicationScoped
+public class MongoProducers {
+    @Produces
+    @ApplicationScoped
+    public static MongoClient client() {
+        CodecRegistry pojoCodecRegistry = fromRegistries(MongoClientSettings.getDefaultCodecRegistry(),
+                                                         fromProviders(PojoCodecProvider.builder()
+                                                                               .automatic(true)
+                                                                               .conventions(Conventions.DEFAULT_CONVENTIONS)
+                                                                               .build()));
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applicationName("shipping")
+                .applyToClusterSettings(
+                        builder -> builder.hosts(Collections.singletonList(new ServerAddress("shipping-db", 27017))))
+                .codecRegistry(pojoCodecRegistry)
+                .build();
+        return MongoClients.create(settings);
+    }
+    
+    @Produces
+    @ApplicationScoped
+    public static MongoDatabase db(MongoClient client) {
+        return client.getDatabase("shipments");
+    }
+
+    @Produces
+    @ApplicationScoped
+    public static MongoCollection<Shipment> shipments(MongoDatabase db) {
+        return db.getCollection("shipments", Shipment.class);
+    }
+}
---
+++ shipping-mongo/src/main/java/io/helidon/examples/sockshop/shipping/mongo/MongoShipmentRepository.java
@@ -0,0 +1,48 @@
+package io.helidon.examples.sockshop.shipping.mongo;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.logging.Logger;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Specializes;
+import javax.inject.Inject;
+
+import io.helidon.examples.sockshop.shipping.DefaultShipmentRepository;
+import io.helidon.examples.sockshop.shipping.Shipment;
+
+import com.mongodb.client.MongoCollection;
+
+import static com.mongodb.client.model.Filters.eq;
+
+/**
+ * @author Aleksandar Seovic  2020.01.21
+ */
+@ApplicationScoped
+@Specializes
+public class MongoShipmentRepository extends DefaultShipmentRepository {
+    private static final Logger LOGGER = Logger.getLogger(MongoShipmentRepository.class.getName());
+
+    @Inject
+    private MongoCollection<Shipment> shipments;
+
+    @Override
+    public Collection<Shipment> getAllShipments() {
+        List<Shipment> results = new ArrayList<>();
+        shipments.find().forEach((Consumer<? super Shipment>) results::add);
+        return results;
+    }
+
+    @Override
+    public Shipment getShipment(String id) {
+        return shipments.find(eq("_id", id)).first();
+    }
+
+    @Override
+    public Shipment addShipment(Shipment shipment) {
+        shipments.insertOne(shipment);
+        return shipment;
+    }
+}
